f9ecac444b07a693f7b618f51d393c3e9dea7f96
ranabadrr <142815658+ranabadrr@users.noreply.github.com>
2024-12-07 13:45:21 +0200

    Add files via upload

---
+++ MPU6050_DMP6.ino
@@ -0,0 +1,146 @@
+#define __STM32F1__
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+
+MPU6050 mpu; // MPU6050 default I2C address is 0x68
+
+//MPU6050 mpu(0x69); //Use for AD0 high
+//MPU6050 mpu(0x68, &Wire1); //Use for AD0 low, but 2nd Wire (TWI/I2C) object.
+
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+int const INTERRUPT_PIN = 2;  // Define the interruption #0 pin
+bool blinkState;
+
+/*---MPU6050 Control/Status Variables---*/
+bool DMPReady = false;  // Set true if DMP init was successful
+uint8_t MPUIntStatus;   // Holds actual interrupt status byte from MPU
+uint8_t devStatus;      // Return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // Expected DMP packet size (default is 42 bytes)
+uint8_t FIFOBuffer[64]; // FIFO storage buffer
+
+/*---Orientation/Motion Variables---*/ 
+Quaternion q;           // [w, x, y, z]         Quaternion container
+VectorInt16 aa;         // [x, y, z]            Accel sensor measurements
+VectorInt16 gy;         // [x, y, z]            Gyro sensor measurements
+VectorFloat gravity;    // [x, y, z]            Gravity vector
+float ypr[3];           // [yaw, pitch, roll]  
+float ypr_mpu[3];
+
+volatile bool MPUInterrupt = false;     // Indicates whether MPU6050 interrupt pin has gone high
+
+void DMPDataReady() {
+  MPUInterrupt = true;
+}
+
+
+#include "ros.h"
+#include "std_msgs/Float32MultiArray.h"
+ros::NodeHandle nh;
+
+std_msgs::Float32MultiArray ypr_mpu_msg;
+ros::Publisher ypr_mpu_pub("mpu_readings", &ypr_mpu_msg);
+
+void setup() {
+
+  nh.initNode();
+  nh.advertise(ypr_mpu_pub);
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(INTERRUPT_PIN, INPUT);
+  
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+    Wire.setClock(400000); // 400kHz I2C clock. Comment on this line if having compilation difficulties
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+  #endif
+  
+  Serial.begin(115200); 
+  while (!Serial);
+
+  /*Initialize device*/
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  
+
+  /*Verify connection*/
+  Serial.println(F("Testing MPU6050 connection..."));
+  if(mpu.testConnection() == false){
+    Serial.println("MPU6050 connection failed");
+    while(true);
+  }
+  else {
+    Serial.println("MPU6050 connection successful");
+  }
+
+
+  /* Initializate and configure the DMP*/
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+  
+  /* Supply your gyro offsets here, scaled for min sensitivity */
+  mpu.setXGyroOffset(0);
+  mpu.setYGyroOffset(0);
+  mpu.setZGyroOffset(0);
+  mpu.setXAccelOffset(0);
+  mpu.setYAccelOffset(0);
+  mpu.setZAccelOffset(0);
+
+  /* Making sure it worked (returns 0 if so) */ 
+  if (devStatus == 0) {
+    mpu.CalibrateAccel(6);  // Calibration Time: generate offsets and calibrate our MPU6050
+    mpu.CalibrateGyro(6);
+    mpu.setDMPEnabled(true);
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), DMPDataReady, RISING);
+    MPUIntStatus = mpu.getIntStatus();
+    DMPReady = true;
+    packetSize = mpu.dmpGetFIFOPacketSize(); 
+  } 
+  else {
+    Serial.print(F("DMP Initialization failed (code ")); //Print the error code
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+  }
+
+  
+  
+}
+
+void loop() {
+  if (!DMPReady) return; // Stop the program if DMP programming fails.
+    
+  /* Read a packet from FIFO */
+  if (mpu.dmpGetCurrentFIFOPacket(FIFOBuffer)) { // Get the Latest packet 
+    #ifdef OUTPUT_READABLE_YAWPITCHROLL
+      /* Display Euler angles in degrees */
+      mpu.dmpGetQuaternion(&q, FIFOBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+      
+      ypr_mpu_msg.data_length = 3; 
+      ypr_mpu_msg.data[0] = ypr_mpu[0];
+      ypr_mpu_msg.data[1] = ypr_mpu[1];
+      ypr_mpu_msg.data[2] = ypr_mpu[2];
+
+      ypr_mpu_pub.publish(&ypr_mpu_msg);
+      
+      Serial.print("MPU Yaw: ");
+      Serial.print(ypr_mpu[0]);
+      Serial.print("\t\tMPU Pitch: ");
+      Serial.print(ypr_mpu[1]);
+      Serial.print("\t\tMPU Roll: ");
+      Serial.println(ypr_mpu[2]);
+   
+
+    #endif
+
+
+
+  /* Blink LED to indicate activity */
+  blinkState = !blinkState;
+  digitalWrite(LED_BUILTIN, blinkState);
+  }
+}
